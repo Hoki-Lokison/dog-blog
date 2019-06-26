9f7b42d659c104b5fb744cb6e80a0afc489d634e
Malia Adamson <sonictardis10@gmail.com>
2019-06-26 12:32:16 -0600

    Finished on LocalPort

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require( "cors" );
 const uuid = require( "uuid" )
 
 var server = express( );
-var port = 8080;
+var port = 3000;
 
 //Data
 var data = require("./data.js")
@@ -17,4 +17,18 @@ server.use (express.json());
 
 server.listen (port, function () {
   console.log(`listening on port ${port}`);
+});
+
+server.get("/blogs", function (req, res) {
+  var response = {
+    blogs: data.blogs
+  };
+    res.json(response);
+
+});
+
+server.post("/blogs", function (req, res) {
+  data.blogs.push(req.body.blog);
+  res.status(201);
+  res.send();
 });
