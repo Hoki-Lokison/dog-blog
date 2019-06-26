572e6c3d366a51d42bceeda599fb1c40f497488d
Malia Adamson <sonictardis10@gmail.com>
2019-06-26 11:44:53 -0600

    first backup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require( "express" );
+const cors = require( "cors" );
+const uuid = require( "uuid" )
+
+var server = express( );
+var port = 8080;
+
+//Data
+var data = require("./data.js")
+
+// Middleware
+server.use( express.urlencoded( {
+    extended: false
+} ) );
+server.use( cors( ) );
+server.use (express.json());
+
+server.listen (port, function () {
+  console.log(`listening on port ${port}`);
+});
